54f77f8a640bab7c54eb90181e50b96c2d4b32ba
vendittogiovanni97 <vendittogiovanni97@hotmail.it>
2025-04-03 17:42:33 +0200

    title pages

--- frontend/src/pages/OrdersPage.tsx
+++ frontend/src/pages/OrdersPage.tsx
@@ -1,7 +1,11 @@
 export default function OrdersPage() {
   return (
     <div className="w-full p-6 pt-24">
-      <div className="w-full max-w-6xl mx-auto bg-white rounded-2xl p-8 shadow-lg"></div>
+      <div className="w-full max-w-6xl mx-auto bg-white rounded-2xl p-8 shadow-lg">
+        <h2 className="text-[var(--primary-color)] text-3xl font-bold">
+          Gestione Ordini
+        </h2>
+      </div>
     </div>
   );
 }
